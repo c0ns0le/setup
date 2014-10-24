1c40594f541503ac4f0ec8a24b7f2061cdd9203a
Brian Jacobowski <bjacobowski.dev@gmail.com>
2014-10-24 11:13:17 -0500

    added various config files

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module "PowerTab" -ArgumentList "C:\Users\Brian\Documents\WindowsPowerShell\PowerTabConfig.xml"
+Import-Module Pscx
+
+#Invoke-BatchFile "${env:VS100COMNTOOLS}..\..\VC\vcvarsall.bat"
+
+# Load Jump-Location profile
+Import-Module 'C:\Users\Brian\Documents\WindowsPowerShell\Modules\Jump.Location\Jump.Location.psd1'
+
+# Load posh-hg example profile
+. 'C:\Users\Brian\Documents\WindowsPowerShell\Modules\posh-hg\profile.example.ps1'
+
+. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
+
+# Load posh-git example profile
+. 'C:\Users\Brian\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+# Load posh-github example profile
+. 'C:\Users\Brian\Documents\WindowsPowerShell\Modules\Posh-GitHub\Posh-GitHub-Profile.ps1'
+
+$Env:WORKON_HOME= $env:USERPROFILE+"\.virtualenvs"
+$Env:PROJECT_HOME= "c:\dev\python"
+Import-Module virtualenvwrapper
+
+
+New-Alias which where.exe
